b6ebd8d9b8655a9f79110c99d310d6eb35588d74
Ray Lu <raylu511@gmail.com>
2021-12-07 15:11:45 +0000

    Cleaned up

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 //write your code here
 const animals =['cheetah','dog', 'cat', 'dodobird', 'bear', 'dolphin'];
+const numbers = [22, 15, 1114, 416, 37, 4]
 
+// 1
 const pluralize = animalArray => animalArray.map(animal => animal + 's');
-console.log(pluralize(animals));
 
+// 2
 const uppercase = animalArray => animalArray.map(animal => {
     const array = animal.split('');
     array[0] = array[0].toUpperCase();
     return array.join('');
-})
+});
 
-console.log(uppercase(animals))
-
+// 3
 const longWords = animalArray => animalArray.filter(animal => animal.length > 3);
 
-console.log(longWords(animals))
-
+// 4
 const shortWords = animalArray => animalArray.filter(animal => animal.length < 5);
 
-console.log(shortWords(animals))
-
+// 5
 const oddLength = animalArray => animalArray.filter(animal => animal.length % 2 === 1);
 
-console.log(oddLength(animals));
-
+// 6
 const longToShort = animalArray => animalArray.sort((a,b) => b.length - a.length);
 
-console.log(longToShort(animals));
-
+// 7
 const sum = numberArray => numberArray.reduce((prev, curr) => prev + curr);
 
-const numbers = [22, 15, 1114, 416, 37, 4]
 
-console.log(sum(numbers));
+console.log(pluralize(animals));    // [ 'cheetahs', 'dogs', 'cats', 'dodobirds', 'bears', 'dolphins' ]
+console.log(uppercase(animals))     // [ 'Cheetah', 'Dog', 'Cat', 'Dodobird', 'Bear', 'Dolphin' ]
+console.log(longWords(animals))     // [ 'cheetah', 'dodobird', 'bear', 'dolphin' ]
+console.log(shortWords(animals))    // [ 'dog', 'cat', 'bear' ]
+console.log(oddLength(animals));    // [ 'cheetah', 'dog', 'cat', 'dolphin' ]
+console.log(longToShort(animals));  // [ 'dodobird', 'cheetah', 'dolphin', 'bear', 'dog', 'cat' ]
+console.log(sum(numbers));          // 1608
